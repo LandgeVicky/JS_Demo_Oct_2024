e4ddf98ad0e001931e582b15f1232f78319ff591
Vickyint1999 <landgevicky242@gmail.com>
2025-02-05 23:48:07 +0530

    Information controller is added.dev02/MVC_JAN_2025/05022025_PracticeMergeCode03

---
+++ MVC_JAN_2025/Controllers/InformationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_JAN_2025.Controllers
+{
+    public class InformationController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home Page";
+            ViewData["Message"] = "Welcome to the Home page!";
+            return View();  // This will render Views/Home/Index.cshtml
+        }
+
+        // GET: /Home/About
+        public IActionResult About()
+        {
+            ViewData["Title"] = "About Page";
+            ViewData["Message"] = "This is the About page.";
+            return View();  // This will render Views/Home/About.cshtml
+        }
+
+    }
+}
