9924e5cf6c91849f6847893d1f450d2ad207172b
Vickyint1999 <landgevicky242@gmail.com>
2025-02-06 23:54:30 +0530

    Project Added MVC Core Using EF

---
+++ Crud_Operation_MVC_Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Crud_Operation_MVC_Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_Operation_MVC_Core.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
