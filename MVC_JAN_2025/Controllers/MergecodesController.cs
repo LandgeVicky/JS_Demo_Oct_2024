01a36f5df008bfc150429dca4b5c7d50b07ed5a3
Vickyint1999 <landgevicky242@gmail.com>
2025-02-06 08:11:19 +0530

    Code commited old branch.dev02/MVC_JAN_2025/05022025_PracticeMergeCode03

---
+++ MVC_JAN_2025/Controllers/MergecodesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_JAN_2025.Controllers
+{
+    public class MergecodesController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to the Sample Index page!";
+            return View();  // Will render Views/Sample/Index.cshtml        }
+        }
+    }
+}
