e2b109c3a05aaca3fae568e4a4929fb8dd2e213d
Vickyint1999 <landgevicky242@gmail.com>
2025-02-06 08:29:02 +0530

    Code commited fronm this branch dev02/MVC_JAN_2025/06022025_PracticeMergeCode04

---
+++ MVC_JAN_2025/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_JAN_2025.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Product List";
+            ViewData["Message"] = "Here are some products:";
+            return View();  // This will render Views/Product/Index.cshtml
+        }
+
+        // GET: /Product/Details/5
+        public IActionResult Details(int id)
+        {
+            ViewData["Title"] = "Product Details";
+            ViewData["Message"] = $"Details for Product ID: {id}";
+            return View();  // This will render Views/Product/Details.cshtml
+        }
+
+        // GET: /Product/Create
+        public IActionResult Create()
+        {
+            ViewData["Title"] = "Create Product";
+            return View();  // This will render Views/Product/Create.cshtml
+        }
+
+    }
+}
